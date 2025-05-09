5d18d5087cd2c4ad41d5515925f332d62c5568ea
Fabio Miranda <fabiom@gmail.com>
2025-05-08 19:15:52 -0500

    separating sandbox and backend tests

--- backend/tests/test_frontend/test_alive.py
+++ backend/tests/test_frontend/test_alive.py
@@ -1,6 +1,6 @@
 def test_backend_server(current_server, sandbox_server, page):
     """Test that the backend server is live."""
-    page.goto(f"{current_server}/liveness")
+    page.goto(f"{current_server}/health")
     page.wait_for_load_state("domcontentloaded")
     page.get_by_text("Backend is live.").wait_for()
     assert "Backend is live." in page.content()
@@ -8,7 +8,7 @@ def test_backend_server(current_server, sandbox_server, page):
 
 def test_sandbox_server(sandbox_server, page):
     """Test that the sandbox server is live."""
-    page.goto(f"{sandbox_server}/liveness")
+    page.goto(f"{sandbox_server}/health")
     page.wait_for_load_state("domcontentloaded")
     page.get_by_text("Sandbox is live.").wait_for()
     assert "Sandbox is live." in page.content()
